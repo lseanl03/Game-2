1ffe27bd35b4647563dc50a234e236de3ce62cf5
LSEANL03 <haov80530@gmail.com>
2023-07-17 03:08:42 +0700

    update

--- Assets/Asset/Script/CharacterCardData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "New Character Card", menuName = "TCG/CharacterCard")]
-public class CharacterCardData : CharacterCardDataBase
-{
-
-}
